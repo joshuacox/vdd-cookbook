8c12965712f08ec9255008c644ffc3b2ca46cffb
Josh Cox <josh@webhosting.coop>
2015-01-31 16:42:41 -0600

    single quotes

--- recipes/vdd_sites.rb
+++ recipes/vdd_sites.rb
@@ -27,7 +27,7 @@ if node["vdd"]["sites"]
     mysql_connection_info = {
       :host => "localhost",
       :username => "root",
-      :password => node["mysql"]["server_root_password"]
+      :password => node['mysql']['server_root_password']
     }
     mysql_database index do
       connection mysql_connection_info
